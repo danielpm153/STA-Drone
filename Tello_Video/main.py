98af2771c40d572e2dcd4e1f251c3c3de27119e6
danielpm153 <daniel.bol153@gmail.com>
2020-10-21 21:55:26 -0200

    sample interface

--- Tello_Video/main.py
+++ Tello_Video/main.py
@@ -3,7 +3,7 @@ from tello_control_ui import TelloUI
 
 import threading
 import Tkinter as tk
-import curses
+#import curses
 from time import sleep
 import socket
 
@@ -14,11 +14,22 @@ class Interface:
         self.tello = tello
         self.root = tk.Tk()
         self.root.wm_title("Data Drone")
+        self.text = str(self.tello.response)
+
+        self.label_aux1 = tk.Label(self.root)
+        self.label_aux1.pack(side="bottom", fill="both",
+                             expand="yes", padx=10, pady=5)
+        self.label_aux2 = tk.Label(self.root, text=self.text)
+        self.label_aux2.pack(side="bottom", fill="both",
+                             expand="yes", padx=10, pady=5)
+        self.label_aux3 = tk.Label(self.root, text=self.text)
+        self.label_aux3.pack(side="bottom", fill="both",
+                             expand="yes", padx=10, pady=5)
 
         #communitation
-        self.stdscr = curses.initscr()
-        curses.noecho()
-        curses.cbreak()
+        #self.stdscr = curses.initscr()
+        #curses.noecho()
+        #curses.cbreak()
 
         local_ip = ''
         local_port = 8890
@@ -31,11 +42,18 @@ class Interface:
 
         self.socket.sendto('command'.encode('utf-8'), tello_adderss)
         self.dados()
+    def report(self, str):
+        pass
+        #self.stdscr.addstr(0, 0, str)
+        #self.stdscr.refresh()
     def str2dict(self, out):
         dictionnaire = {}
         for i in out:
-            aux = i.split(':')
-            dictionnaire[aux[0]] = aux[1]
+            if ':' in i:
+                aux = i.split(':')
+                dictionnaire[aux[0]] = aux[1]
+            else:
+                pass
         return dictionnaire
 
     def dados(self):
@@ -45,7 +63,16 @@ class Interface:
                 self.root.after(100, self.dados)
             else:
                 self.out = self.response.split(';')
-
+                self.out_dict = self.str2dict(self.out)
+                self.label_aux1["text"] = 'agx =' + self.out_dict['agx']
+                self.label_aux2["text"] = 'agy =' + self.out_dict['agy']
+                self.label_aux3["text"] = 'agz =' + self.out_dict['agz']
+                self.root.after(100, self.dados)
+        except KeyboardInterrupt:
+            pass
+            #curses.echo()
+            #curses.nocbreak()
+            #curses.endwin()
 
 
 
